d1019d025fef05ab773f3c95196dbf6a8701751e
skylove0911 <skylove0911@naver.com>
2023-04-18 17:57:47 +0900

    taek: test1 update

--- ssafy-star/src/pages/test/Test1.tsx
+++ ssafy-star/src/pages/test/Test1.tsx
@@ -1,13 +1,19 @@
 import * as THREE from "three";
-import { useRef, useState } from "react";
+import { Suspense, useRef, useState } from "react";
 import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import {
+  CameraControls,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
 
 function Box(props: ThreeElements["mesh"]) {
   const ref = useRef<THREE.Mesh>(null!);
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
-  useFrame((state, delta) => (ref.current.rotation.x += delta));
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+  });
   return (
     <mesh
       {...props}
@@ -26,13 +32,27 @@ function Box(props: ThreeElements["mesh"]) {
 export default function Test1() {
   return (
     <div className="w-full h-screen">
-      <Canvas>
-        <OrbitControls />
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
+      <Suspense fallback={null}>
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <PerspectiveCamera makeDefault position={[0, 0, 10]} />
+          <Box position={[-25, 0, 0]} />
+          <Box position={[25, 0, 0]} />
+          <Box position={[-25, 25, 0]} />
+          <Box position={[25, -25, 0]} />
+          <Box position={[-25, 0, 25]} />
+          <Box position={[25, 0, -25]} />
+          <Box position={[-25, 0, 0]} />
+          <Box position={[10, 10, 10]} />
+          <Box position={[-25, 25, 25]} />
+          <Box position={[10, -10, 10]} />
+          <Box position={[-10, -10, 10]} />
+          <Box position={[10, 100, -0]} />
+
+          <OrbitControls enableZoom={false} />
+        </Canvas>
+      </Suspense>
     </div>
   );
 }
