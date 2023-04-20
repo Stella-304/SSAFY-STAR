e614dcc619208b79af1a9796ff8b09f36664a943
Garden1298 <20181298@vision.hoseo.edu>
2023-04-21 00:09:33 +0900

    [feat] fusion spawn avater

--- ssafy-star-unity/SsafyStar/Assets/fusion-101-1.1.6/Assets/Fusion102/BasicSpawner.cs
+++ ssafy-star-unity/SsafyStar/Assets/fusion-101-1.1.6/Assets/Fusion102/BasicSpawner.cs
@@ -15,6 +15,7 @@ namespace Fusion102
 		{
 			if (_runner == null)
 			{
+				//사용자가 게임을 호스팅 할 것인지 아니면 기존 세션에 참여할 것인지를 선택
 				if (GUI.Button(new Rect(0,0,200,40), "Host"))
 				{
 					StartGame(GameMode.Host);
@@ -29,15 +30,18 @@ namespace Fusion102
 		async void StartGame(GameMode mode)
 		{
 			// Create the Fusion runner and let it know that we will be providing user input
+			//input 제공
 			_runner = gameObject.AddComponent<NetworkRunner>();
 			_runner.ProvideInput = true;
-	    
+
 			// Start or join (depends on gamemode) a session with a specific name
+			//게임 방 정보 생성
 			await _runner.StartGame(new StartGameArgs()
 			{
 				GameMode = mode, 
 				SessionName = "TestRoom", 
 				Scene = SceneManager.GetActiveScene().buildIndex,
+				//씬에 직접 배치된 NetworkObject의 인스턴스화 처
 				SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
 			});
 		}
@@ -45,16 +49,19 @@ namespace Fusion102
 		[SerializeField] private NetworkPrefabRef _playerPrefab; // Character to spawn for a joining player
 		private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
 
+		//플레이어가 연결할 때, OnPlayerJoined 메소드가 호출
 		public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
 		{
 			if (runner.IsServer)
 			{
 				Vector3 spawnPosition = new Vector3((player.RawEncoded%runner.Config.Simulation.DefaultPlayers)*3,1,0);
+				//유니티의 Instantiate() 메소드를 마지막 매개 변수를 제외하고 유사한 매개 변수 집합을 사용하는 runner.Spawn
 				NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
 				_spawnedCharacters.Add(player, networkPlayerObject);
 			}
 		}
 
+		//플레이어가 연결 해제할 때, OnPlayerLeft 메소드가 호출
 		public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
 		{
 			if (_spawnedCharacters.TryGetValue(player, out NetworkObject networkObject))
@@ -64,6 +71,7 @@ namespace Fusion102
 			}
 		}
 
+		//OnInput 콜백에서 Fusion에 의해 폴링 될 때 클라이언트는 사용자로부터 입력을 수집해야 한다.
 		public void OnInput(NetworkRunner runner, NetworkInput input)
 		{
 			var data = new NetworkInputData();
