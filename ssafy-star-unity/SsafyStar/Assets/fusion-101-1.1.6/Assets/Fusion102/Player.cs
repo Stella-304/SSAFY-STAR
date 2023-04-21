1160f8f1f5b0732ebed8cd2b5be50f072fbe4a7e
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2023-04-21 17:56:43 +0900

    [feat] spawn Ball

--- ssafy-star-unity/SsafyStar/Assets/fusion-101-1.1.6/Assets/Fusion102/Player.cs
+++ ssafy-star-unity/SsafyStar/Assets/fusion-101-1.1.6/Assets/Fusion102/Player.cs
@@ -3,39 +3,51 @@ using UnityEngine;
 
 namespace Fusion102
 {
-	public class Player : NetworkBehaviour
-	{
-		private NetworkCharacterControllerPrototype _cc;
+    public class Player : NetworkBehaviour
+    {
+        [SerializeField]
+        private Ball _prefabBall;
 
-		[SerializeField]
-		private Ball _prefabBall;
+        [Networked]
+        private TickTimer delay { get; set; }
 
-		private Vector3 _forward;
+        private NetworkCharacterControllerPrototype _cc;
+        private Vector3 _forward;
 
-		private void Awake()
-		{
-			_cc = GetComponent<NetworkCharacterControllerPrototype>();
-		}
+        private void Awake()
+        {
+            _cc = GetComponent<NetworkCharacterControllerPrototype>();
+            _forward = transform.forward;
+        }
 
-		public override void FixedUpdateNetwork()
-		{
-			if (GetInput(out NetworkInputData data))
-			{
-				data.direction.Normalize();
-				_cc.Move(5*data.direction*Runner.DeltaTime);
+        public override void FixedUpdateNetwork()
+        {
+            if (GetInput(out NetworkInputData data))
+            {
+                data.direction.Normalize();
+                _cc.Move(5 * data.direction * Runner.DeltaTime);
 
-				if(data.direction.sqrMagnitude>0)
-				{
+                if (data.direction.sqrMagnitude > 0)
+                {
                     _forward = data.direction;
                 }
 
-				if((data.buttons & NetworkInputData.MOUSEBUTTON1)!= 0)
-				{
-					Runner.Spawn(_prefabBall,
-								 transform.position + _forward, Quaternion.LookRotation(_forward),
-								 Object.InputAuthority);
-				}
-			}
-		}
-	}
+                if (delay.ExpiredOrNotRunning(Runner))
+                {
+                    if ((data.buttons & NetworkInputData.MOUSEBUTTON1) != 0)
+                    {
+                        delay = TickTimer.CreateFromSeconds(Runner, 0.5f);
+
+                        Debug.Log("º¼ »ý¼º");
+                        Runner.Spawn(_prefabBall,
+                                     transform.position + _forward, Quaternion.LookRotation(_forward),
+                                     Object.InputAuthority, (runner, o) =>
+                                     {
+                                         o.GetComponent<Ball>().Init(); 
+                                     });
+                    }
+                }
+            }
+        }
+    }
 }
