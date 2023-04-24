c19cab57dd974101efe86bd79a1317375d648ee0
repeater1384 <repeater1384@naver.com>
2023-04-24 13:58:55 +0900

    [feat] company-data.json 추가

---
+++ BackEnd/src/main/java/com/ssafy/star/common/db/entity/CompanyKind.java
@@ -0,0 +1,33 @@
+package com.ssafy.star.common.db.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CompanyKind {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column
+	private String kind;
+
+	@ElementCollection
+	private Set<String> compnayList = new HashSet<String>();
+}
