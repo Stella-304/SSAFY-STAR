c2cfa8e85f0a1bd38defd913d82fa34d42584384
서준배 <sjb378@me.com>
2023-04-24 16:25:00 +0900

    [feat] 회원가입 css적용

--- FrontEnd/src/pages/Signup.tsx
+++ FrontEnd/src/pages/Signup.tsx
@@ -5,18 +5,22 @@ import EarthLayout from "../components/Layout/EarthLayout";
 export default function Signup() {
   return (
     <EarthLayout>
-      <div>
-        <h1>Register</h1>
-        <span>SsafyStar를 사용하기 위해 회원가입 해 주세요</span>
-      </div>
-      <div>
-        <Input id="name" type="input" label="이름" />
-        <Input id="email" type="input" label="이메일" />
-        <Input id="password1" type="password" label="비밀번호" />
-        <Input id="password2" type="password" label="비밀번호 확인" />
-      </div>
-      <div>
-        <BigButton value="회원가입" />
+      <div className="flex flex-col justify-around h-full">
+        <div>
+          <span className="text-4xl block font-bold">Register</span>
+          <span className="text-sm block">
+            SsafyStar를 사용하기 위해 회원가입 해 주세요
+          </span>
+        </div>
+        <div>
+          <Input id="name" type="input" label="이름" />
+          <Input id="email" type="input" label="이메일" />
+          <Input id="password1" type="password" label="비밀번호" />
+          <Input id="password2" type="password" label="비밀번호 확인" />
+        </div>
+        <div className="flex justify-center">
+          <BigButton value="회원가입" />
+        </div>
       </div>
     </EarthLayout>
   );
