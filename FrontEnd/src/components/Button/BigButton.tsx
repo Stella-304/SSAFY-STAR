d676822f495f341109b42c2000d0bca5cc27c37c
서준배 <sjb378@me.com>
2023-04-24 17:42:17 +0900

    [feat] 버튼의 마우스 커서모양 수정

--- FrontEnd/src/components/Button/BigButton.tsx
+++ FrontEnd/src/components/Button/BigButton.tsx
@@ -3,7 +3,7 @@ interface props {
 }
 export default function BigButton({ value }: props) {
   return (
-    <div className="rounded-5 bg-black text-white text-center w-360 h-32 leading-32">
+    <div className="rounded-5 bg-black text-white text-center w-360 h-32 leading-32 cursor-pointer">
       {value}
     </div>
   );
--- FrontEnd/src/components/Button/MidButton.tsx
+++ FrontEnd/src/components/Button/MidButton.tsx
@@ -6,11 +6,11 @@ export default function MidButton({ type, value }: props) {
   return (
     <>
       {type === "outline" ? (
-        <div className="rounded-5 border-solid	border-2 border-blue-400 text-blue-400 text-center w-172 h-32 leading-32">
+        <div className="rounded-5 border-solid	border-2 border-blue-400 text-blue-400 text-center w-172 h-32 leading-32 cursor-pointer">
           {value}
         </div>
       ) : (
-        <div className="rounded-5 border-solid bg-blue-400 text-white text-center w-172 h-32 leading-32">
+        <div className="rounded-5 border-solid bg-blue-400 text-white text-center w-172 h-32 leading-32 cursor-pointer">
           {value}
         </div>
       )}
