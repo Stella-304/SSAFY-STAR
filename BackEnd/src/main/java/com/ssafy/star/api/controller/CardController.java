af97d0f53c4b14c06e8bf7a006065061492280e1
이상학 <dltkdgkr_@naver.com>
2023-04-25 20:42:41 +0900

    [chore] kakao/naver client key

--- BackEnd/src/main/java/com/ssafy/star/api/controller/CardController.java
+++ BackEnd/src/main/java/com/ssafy/star/api/controller/CardController.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
