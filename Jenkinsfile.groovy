71a32e8bb92364193480bb662d04ab46c0e46f17
이상학 <dltkdgkr_@naver.com>
2023-04-25 12:39:59 +0900

    [chore] jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,61 @@
+pipeline {
+  agent any
+
+  stages {
+      
+    stage('connect test') {
+      steps {
+        sh "echo init"
+      }
+    }
+      
+    stage('Build Backend') {
+      steps {
+        sh "echo build"
+        sh "cd ${env.WORKSPACE}/BackEnd && chmod +x ./gradlew && ./gradlew build"
+      }
+    }
+
+    stage('Remove Containers') {
+      steps {
+        sh 'docker ps -aq --filter "name=springboot" | xargs docker stop'
+        sh 'docker ps -aq --filter "name=springboot" | xargs docker rm'
+        sh 'docker ps -aq --filter "name=react" | xargs docker stop'
+        sh 'docker ps -aq --filter "name=react" | xargs docker rm'
+      }
+    }
+
+    stage('Build Springboot Image') {
+      steps {
+        script {
+          def backendDir = "${env.WORKSPACE}/BackEnd"
+          def dockerfile = "${backendDir}/Dockerfile"
+
+          docker.build("springboot-image", "-f ${dockerfile} ${backendDir}")
+        }
+      }
+    }
+
+    stage('Build React.JS Image') {
+      steps {
+        script {
+          def frontendDir = "${env.WORKSPACE}/FrontEnd"
+          def dockerfile = "${frontendDir}/Dockerfile"
+
+          docker.build("react-image", "-f ${dockerfile} ${frontendDir}")
+        }
+      }
+    }
+
+    stage('Run Containers') {
+      steps {
+        script {
+          docker.networks.create('ssafystar-network')
+
+          docker.image('springboot-image').run("-d --network ssafystar-network --name springboot -p 8080:8080")
+          docker.image('react-image').run("-d --network ssafystar-network --name react -p 3000:3000")
+        }
+      }
+    }
+  }
+}
