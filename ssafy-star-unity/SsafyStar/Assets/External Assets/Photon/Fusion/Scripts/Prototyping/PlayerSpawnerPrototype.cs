8027b8c209b07d0e82285e2820fdc21300d4e966
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2023-04-26 11:09:02 +0900

    [feat] rename gameobjects

--- ssafy-star-unity/SsafyStar/Assets/External Assets/Photon/Fusion/Scripts/Prototyping/PlayerSpawnerPrototype.cs
+++ ssafy-star-unity/SsafyStar/Assets/External Assets/Photon/Fusion/Scripts/Prototyping/PlayerSpawnerPrototype.cs
@@ -4,30 +4,34 @@ using Fusion;
 /// <summary>
 /// Prototyping component for spawning Player avatars.
 /// </summary>
-[SimulationBehaviour(Stages = SimulationStages.Forward, Modes =  SimulationModes.Server | SimulationModes.Host)]
-public class PlayerSpawnerPrototype : SpawnerPrototype<PlayerSpawnPointPrototype>, IPlayerJoined, IPlayerLeft, ISceneLoadDone {
+[SimulationBehaviour(Stages = SimulationStages.Forward, Modes = SimulationModes.Server | SimulationModes.Host)]
+public class PlayerSpawnerPrototype : SpawnerPrototype<PlayerSpawnPointPrototype>, IPlayerJoined, IPlayerLeft, ISceneLoadDone
+{
 
 #if UNITY_EDITOR
 
-  protected virtual void Reset() {
-    var protoPlayer = FusionPrototypingPrefabs.BasicPlayer;
-    if (protoPlayer)
-      PlayerPrefab = protoPlayer.GetComponent<NetworkObject>();
-  }
+    protected virtual void Reset()
+    {
+        var protoPlayer = FusionPrototypingPrefabs.BasicPlayer;
+        if (protoPlayer)
+            PlayerPrefab = protoPlayer.GetComponent<NetworkObject>();
+    }
 
 
-  [BehaviourButtonAction("Add Player Spawn Point Manager", false, true, nameof(_spawnPointManagerMissing))]
-  private void InspectorWarnMissingSpawnPointManager() {
-    AddBehaviour<PlayerSpawnPointManagerPrototype>();
-  }
+    [BehaviourButtonAction("Add Player Spawn Point Manager", false, true, nameof(_spawnPointManagerMissing))]
+    private void InspectorWarnMissingSpawnPointManager()
+    {
+        AddBehaviour<PlayerSpawnPointManagerPrototype>();
+    }
 
 #endif
 
-  protected override void RegisterPlayerAndObject(PlayerRef player, NetworkObject playerObject) {
-    base.RegisterPlayerAndObject(player, playerObject);
+    protected override void RegisterPlayerAndObject(PlayerRef player, NetworkObject playerObject)
+    {
+        base.RegisterPlayerAndObject(player, playerObject);
 
-    Runner.SetPlayerObject(player, playerObject);
-  }
+        Runner.SetPlayerObject(player, playerObject);
+    }
 
 }
 
