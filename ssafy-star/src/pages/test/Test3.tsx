1b806ce32b69ffc51890dcb90027c9280e723d16
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2023-05-01 11:17:40 +0900

    [feat] webGL Build & added

--- ssafy-star/src/pages/test/Test3.tsx
+++ ssafy-star/src/pages/test/Test3.tsx
@@ -1,38 +1,11 @@
-import * as THREE from "three";
-import { useRef, useState } from "react";
-import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-
-function Box(props: ThreeElements["mesh"]) {
-  const ref = useRef<THREE.Mesh>(null!);
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-  useFrame((state, delta) => (ref.current.rotation.x += delta));
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-}
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 export default function Test3() {
-  return (
-    <div className="w-full h-screen">
-      <Canvas>
-        <OrbitControls />
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
-    </div>
-  );
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "Build/WebGLFile.loader.js",
+        dataUrl: "Build/WebGLFile.data",
+        frameworkUrl: "Build/WebGLFile.framework.js",
+        codeUrl: "Build/WebGLFile.wasm",
+    });
+    return <Unity unityProvider={unityProvider} style={{ width: 1280, height: 800 }} />;
 }
