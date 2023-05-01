967f60ab227dc1c272bd820121bfe90e5b507f15
서준배 <sjb378@me.com>
2023-05-01 10:51:56 +0900

    [feat] 메인 수정, 소셜로그인 주소 수정

--- FrontEnd/src/pages/User/Oauth.jsx
+++ FrontEnd/src/pages/User/Oauth.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+/// https://k8b304.p.ssafy.io/app/oauth2/authorization/google?redict_uri=
+/// https://k8b304.p.ssafy.io/oauth2/token
+
+/// https://k8b304.p.ssafy.io/app/ << 일단 냅두고
+// https://k8b304.p.ssafy.io/oauth2/token?error=&token=asdsacasc
 export default function Oauth() {
   const navigate = useNavigate();
   useEffect(() => {
--- FrontEnd/src/utils/urls.ts
+++ FrontEnd/src/utils/urls.ts
@@ -2,6 +2,10 @@ const SERVER_API = "https://k8b304.p.ssafy.io";
 
 //로그인
 export const LOGIN_URL = SERVER_API + "/app/api";
+
+//정보 조회
+export const USER_DETAIL_URL = SERVER_API + "/app/user/badge";
+
 //회사명 검색
 export const COMPANY_SEARCH_URL = SERVER_API + "/app/card/company";
 //소셜로그인
