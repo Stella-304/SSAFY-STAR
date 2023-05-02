35641d2a55eb0730a523d14d5a390a58f69ff1d5
이상학 <dltkdgkr_@naver.com>
2023-05-02 10:14:48 +0900

    [chore] qurey syntax error

--- BackEnd/src/main/java/com/ssafy/star/common/db/repository/UserRepository.java
+++ BackEnd/src/main/java/com/ssafy/star/common/db/repository/UserRepository.java
@@ -12,8 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByAccountId(String accountId);
     Optional<User> findByAccountId(String accountId);
-
-    @Query("select u.nickname from user u where id like :id")
+    @Query("select u.nickname from User u where id like :id")
     String findNicknameById(long id);
     Optional<User> findByAccountIdAndEmail(String accountId, String email);
     Optional<User> findByEmail(String email);
