7903f4a96ff557c20118ee57f33c0593b97b942a
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2023-05-04 13:23:00 +0900

    [feat] chat toolkit -> gui & press enter to send & select

--- ssafy-star-unity/SsafyStar/Assets/Scripts/UI/ChatController.cs
+++ ssafy-star-unity/SsafyStar/Assets/Scripts/UI/ChatController.cs
@@ -2,6 +2,10 @@ using System;
 using UnityEngine;
 using Fusion;
 using UnityEngine.UIElements;
+using UnityEngine.UI;
+using TMPro;
+using System.Security.Cryptography;
+using WebGLSupport;
 
 public class ChatController : NetworkBehaviour
 {
@@ -9,9 +13,19 @@ public class ChatController : NetworkBehaviour
     private UIDocument doc;
     private VisualElement chat;
     private VisualElement chatbox;
-    private TextField inputfield;
-    private Label chatText;
+    //private TextField inputfield;
+    //private Label chatText;
     public PlayerMovement player;
+
+    [Header("GUI chatting")]
+    [SerializeField]
+    private GameObject chatContent;
+    [SerializeField]
+    private TMP_InputField inputChat;
+    [SerializeField]
+    private TMP_Text outputChat;
+    [SerializeField]
+    private UnityEngine.UI.Button sendChat;
 
     private bool chatboxVisibility = false;
     public string username = "Default";
@@ -20,33 +34,59 @@ public class ChatController : NetworkBehaviour
     {
         chat = doc.rootVisualElement.Q<VisualElement>("Chat");
         chatbox = doc.rootVisualElement.Q<VisualElement>("chatbox");
-        inputfield = doc.rootVisualElement.Q<TextField>("chatInputField");
-        chatText = doc.rootVisualElement.Q<Label>("chattext");
+        //inputfield = doc.rootVisualElement.Q<TextField>("chatInputField");
+        //chatText = doc.rootVisualElement.Q<Label>("chattext");
 
         chatbox.style.visibility = Visibility.Hidden;
 
         chat.AddManipulator(new Clickable(ChatOnClicked));
 
-        inputfield.RegisterCallback<KeyDownEvent>(e =>
+        //inputfield.RegisterCallback<KeyDownEvent>(e =>
+        //{
+        //    if(e.keyCode.ToString() == "Return")
+        //    {
+        //        Debug.Log("=>"+inputfield.text);
+        //        CallMessageRPC(inputfield.text);
+        //        inputfield.value = "";
+        //    }
+        //});
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
         {
-            if(e.keyCode.ToString() == "Return")
+            inputChat.Select();
+
+            if (inputChat.text == "") return;
+
+            Debug.Log("enter");
+            if (chatboxVisibility)
             {
-                Debug.Log("=>"+inputfield.text);
-                CallMessageRPC(inputfield.text);
-                inputfield.value = "";
+                Debug.Log("send Message");
+                SendMessage();
             }
-        });
+        }
     }
 
     private void ChatOnClicked()
     {
         Debug.Log("clicked");
 
-        chatbox.style.visibility = chatboxVisibility ? Visibility.Hidden : Visibility.Visible;
-        
+        //chatbox.style.visibility = chatboxVisibility ? Visibility.Hidden : Visibility.Visible;
+
+        if (chatboxVisibility)
+        {
+            chatContent.SetActive(false);
+        }
+        else
+        {
+            chatContent.SetActive(true);
+        }
+
         chatboxVisibility = !chatboxVisibility;
 
-        player.stop = chatboxVisibility?true:false;
+        player.stop = chatboxVisibility ? true : false;
     }
 
     public void SetUserName(string text)
@@ -54,15 +94,24 @@ public class ChatController : NetworkBehaviour
         username = text;
     }
 
-    public void CallMessageRPC(string text)
+    public void SendMessage()
     {
-        RPCSendMessage(username, text);
+        Debug.Log(inputChat.text);
+        RPCSendMessage(username, inputChat.text);
+        inputChat.Select();
+        inputChat.text = "";
     }
+
+    //public void CallMessageRPC(string text)
+    //{
+    //    RPCSendMessage(username, text);
+    //}
 
     [Rpc(RpcSources.All, RpcTargets.All)]
     public void RPCSendMessage(string username, string message, RpcInfo rpcInfo = default)
     {
-        Debug.Log("<="+message);
-        chatText.text += $"{username}: {message}\n";
+        Debug.Log("<=" + message);
+        outputChat.text += $"{username}: {message}\n";
+        //chatText.text += $"{username}: {message}\n";
     }
 }
