d64096918fa836250f9864b12fe08bd15e749dea
skylove0911 <skylove0911@naver.com>
2023-05-04 14:58:59 +0900

    [fix] merge conflit 해결

--- FrontEnd/src/components/Card/CardBack.tsx
+++ FrontEnd/src/components/Card/CardBack.tsx
@@ -8,17 +8,14 @@ interface Iprops {
 
 export default function CardBack({ user, onClick }: Iprops) {
   return (
-    <div className="h-full w-full select-none rounded-20 bg-gradient-to-b from-cardTop to-[#EFF6EE] px-25 py-28 shadow-md">
-      <div className="absolute left-0 top-0 h-0 w-0  border-r-30 border-t-30 border-r-transparent border-t-red-500 hover:border-t-red-300"></div>
-      <div className="relative h-full w-full rounded-20 border-2 border-black">
-        <div className="absolute left-[calc(50%-37px)] top-[-15px] h-25 w-75 bg-cardTop text-center text-16 font-bold">
-          - ★ -
-        </div>
-        <img
-          className="absolute left-1/4 top-[33%] aspect-square w-1/2"
-          alt="back"
-          src="/background/cardBackCircle.svg"
-        />
+    <div className="box-border h-640 w-480 rounded-2xl border-5 border-white bg-black bg-opacity-70 px-20 py-20 shadow-neon">
+      <img
+        src="/icons/exit-white.svg"
+        className="absolute right-8 top-8 z-20 h-20 w-20 cursor-pointer"
+        onClick={onClick}
+      />
+      <div className="absolute left-0 top-0 h-0 w-0  border-r-30 border-t-30 border-r-transparent border-t-white hover:border-t-blue-900"></div>
+      <div className="from-opacity-70 to-opacity-38 relative h-full w-full rounded-lg border-5 border-white bg-gradient-to-b from-black to-darkblue shadow-neon">
         <div className="absolute top-25 flex w-full flex-col pl-20">
           <div className="mb-10 text-40 font-bold text-white">{user.name}</div>
           <div className="text-20 text-white">
@@ -65,7 +62,9 @@ export default function CardBack({ user, onClick }: Iprops) {
           )}
           {user.blogAddr && (
             <div className="flex h-36 items-center gap-10">
-              <div className="h-36 text-24  font-bold">기술 블로그</div>
+              <div className="h-36 text-24  font-bold text-white">
+                기술 블로그
+              </div>
               <Link
                 to={user.blogAddr}
                 target="_blank"
@@ -78,20 +77,26 @@ export default function CardBack({ user, onClick }: Iprops) {
 
           {user.bojTier && (
             <div className="flex h-36 items-center gap-10">
-              <div className="h-36 text-24  font-bold">백준 티어</div>
-              <div className="h-36 text-22 ">{user.bojTier}</div>
+              <div className="h-36 text-24  font-bold text-white">
+                백준 티어
+              </div>
+              <div className="h-36 text-22 text-white">{user.bojTier}</div>
             </div>
           )}
           {user.swTier && (
             <div className="flex h-36 items-center gap-10">
-              <div className="h-36 text-24  font-bold">역량 테스트</div>
-              <div className="h-36 text-22 ">{user.swTier}</div>
+              <div className="h-36 text-24  font-bold text-white">
+                역량 테스트
+              </div>
+              <div className="h-36 text-22 text-white">{user.swTier}</div>
             </div>
           )}
           {user.etc && (
             <div className="flex h-36 items-center gap-10">
-              <div className="text-24 font-bold">기타사항</div>
-              <div className="whitespace-pre-wrap text-22">{user.etc}</div>
+              <div className="text-24 font-bold text-white">기타사항</div>
+              <div className="whitespace-pre-wrap text-22 text-white">
+                {user.etc}
+              </div>
             </div>
           )}
         </div>
