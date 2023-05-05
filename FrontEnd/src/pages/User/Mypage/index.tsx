11211c03405ebfd8d17633ac4dca750ba2569056
deokisys <sjb378@me.com>
2023-05-05 22:46:55 +0900

    [feat] 마이페이지 닉네임 표시

--- FrontEnd/src/pages/User/Mypage/index.tsx
+++ FrontEnd/src/pages/User/Mypage/index.tsx
@@ -3,22 +3,26 @@ import SmallButton from "../../../components/Button/SmallButton";
 import EarthLayout from "../../../components/Layout/EarthLayout";
 import InfoModi from "./InfoModi";
 import Certify from "./Certify";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../stores/store";
 export default function Mypage() {
   const [page, setPage] = useState(0);
+  const { nickname } = useSelector((state: RootState) => state.user);
 
   return (
     <EarthLayout>
-      <div className="flex flex-col justify-around h-full">
+      <div className="flex h-full flex-col justify-around">
         <div>
-          <span className="text-4xl block font-bold">Mypage</span>
+          <span className="block text-4xl font-bold">Mypage</span>
+          <span className="block text-sm font-bold">
+            {nickname}님 어서오세요!
+          </span>
         </div>
         <div className="mt-8 flex justify-around">
-          <SmallButton value="정보수정" onClick={()=>setPage(0)}/>
-          <SmallButton value="인증"  onClick={()=>setPage(1)}/>
+          <SmallButton value="정보수정" onClick={() => setPage(0)} />
+          <SmallButton value="인증" onClick={() => setPage(1)} />
         </div>
-        {page===0?<InfoModi />:<Certify/>}
-        
-
+        {page === 0 ? <InfoModi /> : <Certify />}
       </div>
     </EarthLayout>
   );
