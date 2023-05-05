810139cbce65890a5dc4547265e4fba5fe921a1b
Garden1298 <20181298@vision.hoseo.edu>
2023-05-05 20:29:44 +0900

    [fix] tool kit chat -> gui 완전 수정

--- ssafy-star-unity/SsafyStar/Assets/Scripts/UI/ChatController.cs
+++ ssafy-star-unity/SsafyStar/Assets/Scripts/UI/ChatController.cs
@@ -12,9 +12,6 @@ public class ChatController : NetworkBehaviour
     [SerializeField]
     private UIDocument doc;
     private VisualElement chat;
-    private VisualElement chatbox;
-    //private TextField inputfield;
-    //private Label chatText;
     public PlayerMovement player;
 
     [Header("GUI chatting")]
@@ -33,23 +30,8 @@ public class ChatController : NetworkBehaviour
     private void Start()
     {
         chat = doc.rootVisualElement.Q<VisualElement>("Chat");
-        chatbox = doc.rootVisualElement.Q<VisualElement>("chatbox");
-        //inputfield = doc.rootVisualElement.Q<TextField>("chatInputField");
-        //chatText = doc.rootVisualElement.Q<Label>("chattext");
-
-        chatbox.style.visibility = Visibility.Hidden;
 
         chat.AddManipulator(new Clickable(ChatOnClicked));
-
-        //inputfield.RegisterCallback<KeyDownEvent>(e =>
-        //{
-        //    if(e.keyCode.ToString() == "Return")
-        //    {
-        //        Debug.Log("=>"+inputfield.text);
-        //        CallMessageRPC(inputfield.text);
-        //        inputfield.value = "";
-        //    }
-        //});
     }
 
     public void Update()
@@ -72,8 +54,6 @@ public class ChatController : NetworkBehaviour
     private void ChatOnClicked()
     {
         Debug.Log("clicked");
-
-        //chatbox.style.visibility = chatboxVisibility ? Visibility.Hidden : Visibility.Visible;
 
         if (chatboxVisibility)
         {
@@ -102,16 +82,10 @@ public class ChatController : NetworkBehaviour
         inputChat.text = "";
     }
 
-    //public void CallMessageRPC(string text)
-    //{
-    //    RPCSendMessage(username, text);
-    //}
-
     [Rpc(RpcSources.All, RpcTargets.All)]
     public void RPCSendMessage(string username, string message, RpcInfo rpcInfo = default)
     {
         Debug.Log("<=" + message);
         outputChat.text += $"{username}: {message}\n";
-        //chatText.text += $"{username}: {message}\n";
     }
 }
