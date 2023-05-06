16678293dea99ef088a46b2dffab0707c068c54e
Garden1298 <20181298@vision.hoseo.edu>
2023-05-06 14:26:41 +0900

    [feat] 채팅 속성 UI 추가

--- ssafy-star-unity/SsafyStar/Assets/Scripts/UI/ChatController.cs
+++ ssafy-star-unity/SsafyStar/Assets/Scripts/UI/ChatController.cs
@@ -7,6 +7,7 @@ using TMPro;
 using System.Security.Cryptography;
 using WebGLSupport;
 
+public enum ChatType { Normal = 0, Party, Guild, Whisper, System, Count}
 public class ChatController : NetworkBehaviour
 {
     [SerializeField]
@@ -28,8 +29,24 @@ public class ChatController : NetworkBehaviour
     [SerializeField]
     private Transform parentContent;
 
+    [Header("chat type")]
+    [SerializeField]
+    private Sprite[] spriteChatInputType; // 대화 속성에 해당하는 이미지 에셋
+    [SerializeField]
+    private UnityEngine.UI.Image imageChatInputType; // 대화 속성 이미지
+    [SerializeField]
+    private TextMeshProUGUI textInput;
+    private ChatType currentInputType; // 현재 대화 속성
+    private Color currentTextColor; // 입력에 따라 색상 변환
+
     private bool chatboxVisibility = false;
     public string username = "Guest";
+
+    private void Awake()
+    {
+        currentInputType = ChatType.Normal;
+        currentTextColor = Color.white;
+    }
 
     private void Start()
     {
@@ -62,6 +79,11 @@ public class ChatController : NetworkBehaviour
 
             //대화 입력창 포커스 활성화;
             inputChat.ActivateInputField();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Tab)&&chatboxVisibility)
+        {
+            SetCurrentInputType();
         }
     }
 
@@ -102,8 +124,30 @@ public class ChatController : NetworkBehaviour
         Debug.Log("<=" + message);
 
         GameObject clone = Instantiate(textChatPrefab, parentContent);
-        clone.GetComponent<TextMeshProUGUI>().text = $"{username}: {message}\n";
+        ChatCell cell = clone.GetComponent<ChatCell>();
 
-        //outputChat.text += $"{username}: {message}\n";
+        //clone.GetComponent<TextMeshProUGUI>().text = $"{username}: {message}\n";
+        cell.SetUp(currentInputType, currentTextColor, $"{username}: {message}\n");
+    }
+
+    private Color ChatTypeToColor(ChatType type)
+    {
+        Color[] colors = new Color[(int)ChatType.Count] {
+            Color.black, Color.blue, Color.green, Color.magenta, Color.yellow
+        };
+
+        return colors[(int)type];
+    }
+
+    public void SetCurrentInputType()
+    {
+        //현재 대화 속성을 한 단계씩 변화(귓말, 시스템은 입력 속성에 없기 때문에 제외)
+        currentInputType = (int)currentInputType < (int)ChatType.Count - 3 ? currentInputType + 1 : 0;
+        //버튼 이미지 변경
+        imageChatInputType.sprite = spriteChatInputType[(int)currentInputType];
+        //텍스트 색상 변경
+        currentTextColor = ChatTypeToColor(currentInputType);
+        //대화 입력창의 텍스트 생상 변경
+        textInput.color = currentTextColor;
     }
 }
