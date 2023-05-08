c14fe584119578d49d211af1db4397a0a43aaa05
deokisys <sjb378@me.com>
2023-05-09 08:42:20 +0900

    [feat] 헤더 형태 조정

---
+++ FrontEnd/src/components/Button/HeaderButton.tsx
@@ -0,0 +1,26 @@
+interface props {
+  value: string;
+  path: boolean;
+  onClick?: () => void;
+}
+export default function HeaderButton({ value, onClick, path }: props) {
+  return (
+    <>
+      {path ? (
+        <button
+          className="h-44 w-100 rounded-4 bg-yellow-400"
+          onClick={onClick}
+        >
+          {value}
+        </button>
+      ) : (
+        <button
+          className="h-44 w-100 rounded-4 bg-yellow-300"
+          onClick={onClick}
+        >
+          {value}
+        </button>
+      )}
+    </>
+  );
+}
--- FrontEnd/src/components/Layout/HeaderMenu.tsx
+++ FrontEnd/src/components/Layout/HeaderMenu.tsx
@@ -3,7 +3,7 @@ import useLogout from "../../apis/user/useLogout";
 import { useSelector } from "react-redux";
 import { RootState } from "../../stores/store";
 import { useEffect, useState } from "react";
-import FloatButton from "../Button/FloatButton";
+import HeaderButton from "../Button/HeaderButton";
 
 export default function FloatingMenu() {
   const { email, cardRegistered } = useSelector(
@@ -31,40 +31,40 @@ export default function FloatingMenu() {
   }
   return (
     <>
-      <div className="fixed left-0 top-0 flex w-full justify-between gap-8 bg-black font-['nemo030']">
+      <div className="fixed left-0 top-0 flex w-full h-44 justify-between items-center gap-8 bg-black font-['nemo030']">
         <div
-          className="cursor-pointer select-none text-center font-['RocaOne'] text-4xl text-[#02C7FE]"
+          className="ml-8 cursor-pointer select-none text-center font-['RocaOne'] text-4xl text-[#02C7FE]"
           onClick={() => navigate("/")}
         >
           SSAFY-STAR
         </div>
         <div className="flex justify-end gap-8">
-          <FloatButton
+          <HeaderButton
             onClick={() => navigate("/universe")}
             value="유니버스"
             path={path === "universe"}
           />
-          <FloatButton
+          <HeaderButton
             onClick={() => navigate("/metaverse")}
             value="메타버스"
             path={path === "metaverse"}
           />
           {email ? (
             <>
-              <FloatButton onClick={logout} value="로그아웃" path={false} />
-              <FloatButton
+              <HeaderButton onClick={logout} value="로그아웃" path={false} />
+              <HeaderButton
                 onClick={() => navigate("/mypage")}
                 value="마이페이지"
                 path={path === "mypage"}
               />
               {cardRegistered ? (
-                <FloatButton
+                <HeaderButton
                   onClick={() => navigate("/cardsubmit/modify")}
                   value="카드 수정"
                   path={path === "cardmodify"}
                 />
               ) : (
-                <FloatButton
+                <HeaderButton
                   onClick={() => navigate("/cardsubmit/submit")}
                   value="카드 등록"
                   path={path === "cardsubmit"}
@@ -73,12 +73,12 @@ export default function FloatingMenu() {
             </>
           ) : (
             <>
-              <FloatButton
+              <HeaderButton
                 onClick={() => navigate("/login")}
                 value="로그인"
                 path={path === "login"}
               />
-              <FloatButton
+              <HeaderButton
                 onClick={() => navigate("/signup")}
                 value="회원가입"
                 path={path === "signup"}
