3a472764019e4eff474f36ecbc69d94dedd20882
skylove0911 <skylove0911@naver.com>
2023-05-09 17:57:12 +0900

    [feat] 유니버스 배경음악 추가

--- FrontEnd/src/components/Card/CardPreviewFront.tsx
+++ FrontEnd/src/components/Card/CardPreviewFront.tsx
@@ -5,12 +5,19 @@ interface Iprops {
   name: string;
 }
 
-export default function CardPreviewFront({ generation, campus, ban, name }: Iprops) {
+export default function CardPreviewFront({
+  generation,
+  campus,
+  ban,
+  name,
+}: Iprops) {
   return (
-    <div className="box-border px-10 py-10 w-full h-full bg-opacity-70 bg-black border-3 border-white shadow-neon rounded-20">
-      <div className="relative h-full w-full bg-gradient-to-b from-opacity-70 to-opacity-38 from-black to-darkblue border-3 border-white shadow-neon rounded-15">
+    <div className="box-border h-full w-full rounded-20 border-3 border-white bg-black bg-opacity-70 px-10 py-10 shadow-neon">
+      <div className="from-opacity-70 to-opacity-38 relative h-full w-full rounded-15 border-3 border-white bg-gradient-to-b from-black to-darkblue shadow-neon">
         <div className="absolute top-1/3 flex w-full flex-col items-center justify-center text-center">
-          <div className="text-14 font-bold text-white">- {generation}기 {campus} {ban}반 -</div>
+          <div className="text-14 font-bold text-white">
+            {generation}기 {campus} {ban}반
+          </div>
           <div className="mt-15 text-20 font-bold text-white">{name}</div>
         </div>
       </div>
