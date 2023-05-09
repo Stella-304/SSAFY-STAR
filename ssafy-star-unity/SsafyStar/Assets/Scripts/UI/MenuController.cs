ddb2893974299537887301c4198e67e21f7920f7
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2023-05-09 17:37:55 +0900

    [fix] menu ui fix

--- ssafy-star-unity/SsafyStar/Assets/Scripts/UI/MenuController.cs
+++ ssafy-star-unity/SsafyStar/Assets/Scripts/UI/MenuController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MenuController : MonoBehaviour
 {
@@ -15,17 +16,17 @@ public class MenuController : MonoBehaviour
     
     [Header("Setting Button")]
     private VisualElement btnWrapper;
-    [SerializeField]
-    private VisualTreeAsset btnSettingTemplate;
-    private VisualElement btnSettings;
+    //[SerializeField]
+    //private VisualTreeAsset btnSettingTemplate;
+    //private VisualElement btnSettings;
 
     private UIDocument doc;
     private Button btnPlay;
-    private Button btnExit;
-    private Button btnSetting;
+    //private Button btnExit;
+    //private Button btnSetting;
     private Button btnMute;
     private VisualElement cardPlay;
-    private VisualElement cardOther;
+    private VisualElement cardGuest;
 
 
     private void Awake()
@@ -33,32 +34,31 @@ public class MenuController : MonoBehaviour
         doc = GetComponent<UIDocument>();
 
         btnPlay = doc.rootVisualElement.Q<Button>("ButtonPlay");
-        btnExit = doc.rootVisualElement.Q<Button>("ButtonExit");
-        btnSetting = doc.rootVisualElement.Q<Button>("ButtonSetting");
         btnMute = doc.rootVisualElement.Q<Button>("ButtonMute");
         cardPlay = doc.rootVisualElement.Q<VisualElement>("CardPlay");
-        cardOther = doc.rootVisualElement.Q<VisualElement>("CardOther");
+        cardGuest = doc.rootVisualElement.Q<VisualElement>("CardGuest");
 
         btnWrapper = doc.rootVisualElement.Q<VisualElement>("Buttons");
 
-        btnPlay.clicked += BtnPlayOnClicked;//()=>{ÇÔ¼ö();};
-        btnExit.clicked += BtnExitOnClicked;
+        cardPlay.AddManipulator(new Clickable(BtnPlayOnClicked));
+        cardGuest.AddManipulator(new Clickable(BtnGuestOnClicked));
         btnMute.clicked += BtnMuteOnClicked;
-        btnSetting.clicked += BtnSettingOnClicked;
 
-        btnSettings = btnSettingTemplate.CloneTree();
-        var btnBack = btnSettings.Q<Button>("ButtonBack");
-        btnBack.clicked += BtnBackOnClickec;
+        //btnSettings = btnSettingTemplate.CloneTree();
+        //var btnBack = btnSettings.Q<Button>("ButtonBack");
+        //btnBack.clicked += BtnBackOnClickec;
     }
 
     private void BtnPlayOnClicked()
     {
-        SceneManager.LoadScene("Lobby");
+        Debug.Log("play");
+        //SceneManager.LoadScene("Lobby");
     }
 
-    private void BtnExitOnClicked()
+    private void BtnGuestOnClicked()
     {
-        //Application.Quit();
+        Debug.Log("guest");
+        //SceneManager.LoadScene("Lobby");
     }
 
     private void BtnMuteOnClicked()
@@ -71,21 +71,21 @@ public class MenuController : MonoBehaviour
         AudioListener.volume = muted ? 0 : 1;
     }
 
-    private void BtnSettingOnClicked()
-    {
-        btnWrapper.Clear();
-        btnWrapper.Add(btnSettings);
-    }
+    //private void BtnSettingOnClicked()
+    //{
+    //    btnWrapper.Clear();
+    //    btnWrapper.Add(btnSettings);
+    //}
 
-    private void BtnBackOnClickec()
-    {
-        btnWrapper.Clear();
+    //private void BtnBackOnClickec()
+    //{
+    //    btnWrapper.Clear();
 
-        btnWrapper.Add(cardPlay);
-        btnWrapper.Add(cardOther);
+    //    btnWrapper.Add(cardPlay);
+    //    btnWrapper.Add(cardOther);
 
-        //btnWrapper.Add(btnPlay);
-        //btnWrapper.Add(btnSetting);
-        //btnWrapper.Add(btnExit);
-    }
+    //    //btnWrapper.Add(btnPlay);
+    //    //btnWrapper.Add(btnSetting);
+    //    //btnWrapper.Add(btnExit);
+    //}
 }
