e8fa1cc3ebd61d10710dda1270f488813c52418d
이상학 <dltkdgkr_@naver.com>
2023-05-10 15:15:50 +0900

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,17 +4,18 @@ pipeline {
   stages {
     stage('Build Backend') {
       steps {
-        sh 'echo build'
+        sh "echo build"
         sh "cd ${env.WORKSPACE}/BackEnd && chmod +x ./gradlew && ./gradlew clean build"
       }
     }
 
     stage('Remove Containers') {
       steps {
-        sh 'docker ps -f name=springboot -q | xargs --no-run-if-empty -r docker container stop'
-        sh 'docker ps -f name=react -q | xargs --no-run-if-empty -r docker container stop'
-        sh 'docker container ls -a -f name=springboot -q | xargs --no-run-if-empty -r docker container rm'
-        sh 'docker container ls -a -f name=react -q | xargs --no-run-if-empty -r docker container rm'
+        sh 'docker ps -f name=springboot-master -q | xargs --no-run-if-empty -r docker container stop'
+        sh 'docker ps -f name=react-master -q | xargs --no-run-if-empty -r docker container stop'
+        sh 'docker container ls -a -f name=springboot-master -q | xargs --no-run-if-empty -r docker container rm'
+        sh 'docker container ls -a -f name=react-master -q | xargs --no-run-if-empty -r docker container rm'
+
       }
     }
 
@@ -25,6 +26,17 @@ pipeline {
       }
     }
 
+    stage('Remove Volume') {
+      steps {
+        sh 'docker volume ssafy-star-volume -q | xargs --no-run-if-empty -r docker volume rm'
+      }
+    }
+
+    stage('Create Volume') {
+      steps {
+        sh 'docker volume create --name ssafy-star-volume -d local --opt type=none --opt device=/usr/share/nginx/html --opt o=bind'
+      }
+    }
 
     stage('Build Springboot Image') {
       steps {
@@ -51,8 +63,8 @@ pipeline {
     stage('Run Containers') {
       steps {
         script {
-          docker.image('springboot-image').run("--name springboot -p 8080:8080")
-          docker.image('react-image').run("-v ssafy-star-volume:/usr/share/nginx/html --name react -p 3000:3000")
+          docker.image('springboot-image-master').run("-d --network ssafystar-network-master --name springboot -p 8081:8081")
+          docker.image('react-image-master').run("-d --network ssafystar-network --name react -p 3001:3001")
         }
       }
     }
