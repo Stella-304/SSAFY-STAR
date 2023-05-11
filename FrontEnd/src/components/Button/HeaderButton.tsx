78c264dabb80dab19246c314dadab3c0ddbe78bb
장재욱 <bomerj@naver.com>
2023-05-11 16:05:14 +0900

    [feat] header menu font modify and favicon modify

--- FrontEnd/src/components/Button/HeaderButton.tsx
+++ FrontEnd/src/components/Button/HeaderButton.tsx
@@ -8,14 +8,14 @@ export default function HeaderButton({ value, onClick, path }: props) {
     <>
       {path ? (
         <button
-          className="h-32 w-100 rounded-4 bg-black text-yellow-500"
+          className="h-32 w-100 rounded-4 bg-black text-[#2f81f7] font-neob"
           onClick={onClick}
         >
           {value}
         </button>
       ) : (
         <button
-          className="h-32 w-100 rounded-4 bg-black text-white hover:text-yellow-400"
+          className="h-32 w-100 rounded-4 bg-black text-white hover:text-[#2f81f7] font-neo"
           onClick={onClick}
         >
           {value}
