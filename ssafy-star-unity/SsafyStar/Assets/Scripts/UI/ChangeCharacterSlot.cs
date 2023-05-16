dd7d5b97b1050772bd6f44af9bda6cd0b7ee0e13
Garden1298 <20181298@vision.hoseo.edu>
2023-05-16 17:24:54 +0900

    [feat] CharacterSelect Button

--- ssafy-star-unity/SsafyStar/Assets/Scripts/UI/ChangeCharacterSlot.cs
+++ ssafy-star-unity/SsafyStar/Assets/Scripts/UI/ChangeCharacterSlot.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ChangeCharacterSlot : MonoBehaviour
 {
     public int CharacterNum;
+
+    public void SelectCharacter()
+    {
+        Debug.Log("클릭했다"+CharacterNum);
+    }
 }
